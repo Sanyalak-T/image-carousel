ec4000217aec3e78827b5fb4d141d1d362822a61
Sanyalak-T <isanyalakt@gmail.com>
2021-11-24 20:58:04 +0700

    second commit

--- script.js
+++ script.js
@@ -17,19 +17,23 @@ function changeImage() {
     if(idx > img.length - 1) {
         idx = 0
     } else if(idx < 0) {
-        idx = img.length -1
+        idx = img.length - 1
     }
+
     imgs.style.transform = `translateX(${-idx * 500}px)`
 }
+
 function resetInterval() {
     clearInterval(interval)
     interval = setInterval(run, 2000)
 }
+
 rightBtn.addEventListener('click', () => {
     idx++
     changeImage()
     resetInterval()
 })
+
 leftBtn.addEventListener('click', () => {
     idx--
     changeImage()
